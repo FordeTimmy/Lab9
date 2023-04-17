9b2883efda04e9e120f66cae8590576df2b966b9
timmy forde <timmyforde02@gmail.com>
2023-04-17 10:37:36 +0100

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
-
+import {Storage} from '@ionic/storage-angular';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-stories:any[]=[];
-  constructor() {}
+myStatus:any="";
+  constructor(private storage:Storage) {}
   
+  async ionViewWillEnter(){
+    this.myStatus = await this.storage.get("Status");
+  }
 }
